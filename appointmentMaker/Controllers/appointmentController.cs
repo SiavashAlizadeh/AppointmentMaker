d2bad0005cba219dbbb35f3d5df8abe3ef479c33
SiavashAlizadeh <siavash.alizadeh98@gmail.com>
2022-08-26 00:45:48 +0430

    first commit

---
+++ appointmentMaker/Controllers/appointmentController.cs
@@ -0,0 +1,24 @@
+﻿using appointmentMaker.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace appointmentMaker.Controllers
+{
+    public class appointmentController : Controller
+    {
+        static List<appointmentModel> appointments = new List<appointmentModel>();
+        public IActionResult Index()
+        {
+            return View(appointments);
+        }
+
+        public IActionResult create()
+        {
+            return View();
+        }
+        public IActionResult showDetails(appointmentModel appointmentDetail)
+        {
+            appointments.Add(appointmentDetail);
+            return View("showDetails", appointmentDetail);
+        }
+    }
+}
---
+++ appointmentMaker/Models/appointmentModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace appointmentMaker.Models
+{
+    public class appointmentModel
+    {
+        [Required]
+        [StringLength(20,MinimumLength =4)]
+
+        [DisplayName("patient's full name")]
+        public String patientName { get; set; }
+
+        [DisplayName("appointment request Data")]
+
+        [DataType(DataType.Date)]
+        public DateTime appointmentDate { get; set; }
+
+        [DisplayName("appointment request net worth")]
+        [DataType(DataType.Currency)]
+        public decimal patientNetWorth { get; set; }
+
+        [DisplayName("appointment request doctor's name")]
+
+        public String doctorsName { get; set; }
+
+        [DisplayName("pain level is (1-10)")]
+        [Range(1,10)]
+        public int painLevel { get; set; }
+    }
+}
