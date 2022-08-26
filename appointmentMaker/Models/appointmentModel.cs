922a6e2bceab9a3df8b51b6740216c0152be55da
BuildTools <wafaaelayoubi.w@gmail.com>
2022-08-26 21:15:02 +0300

    Editing The form
    adding City and Street and fix the validation for currency

--- appointmentMaker/Models/appointmentModel.cs
+++ appointmentMaker/Models/appointmentModel.cs
@@ -11,21 +11,34 @@ namespace appointmentMaker.Models
         [DisplayName("patient's full name")]
         public String patientName { get; set; }
 
-        [DisplayName("appointment request Data")]
+        [DisplayName("City")]
+        [Required]
+        [StringLength(20, MinimumLength = 4)]
+        public string City { get; set; }
 
+        [DisplayName("Street")]
+        [Required]
+        [StringLength(20, MinimumLength = 4)]
+        public string Street { get; set; }
+
+        [DisplayName("appointment request Date")]
         [DataType(DataType.Date)]
         public DateTime appointmentDate { get; set; }
 
         [DisplayName("appointment request net worth")]
+        [Range(90000, 9999999999999999.99 , ErrorMessage = "Your Currency should be above 90k")]
         [DataType(DataType.Currency)]
         public decimal patientNetWorth { get; set; }
 
         [DisplayName("appointment request doctor's name")]
-
         public String doctorsName { get; set; }
 
         [DisplayName("pain level is (1-10)")]
         [Range(1,10)]
         public int painLevel { get; set; }
+
+        
+
+        
     }
 }
